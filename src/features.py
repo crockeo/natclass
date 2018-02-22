6b696aaadd15ce380111e1a93bdd1df34235e1b5
Cerek Hillen <cerekh@gmail.com>
2018-02-22 13:03:44 -0500

    Added place constraints.

--- src/features.py
+++ src/features.py
@@ -6,3 +6,24 @@ def get_features():
         features = json.load(f)
     return features
 get_features.features = {}
+
+###
+# place_constraints
+#
+# A set of constraints that map directly from major class to phonetic place.
+place_constraints = {
+    'Bilabial': '[] => [+labial, -labiodental]',
+    'Labiodental': '[] => [+labidal, +labiodental]',
+
+    'Dental': '[] => [+coronal, +anterior, +distributed, -dorsal]',
+    'Alveolar': '[] => [+coronal, +anterior, -distributed]',
+    'Postalveolar': '[] => [+coronal, -anterior, +distributed]',
+    'Retroflex': '[] => [+coronal, -anterior, -distributed]',
+    'Alveolopalatal': '[+coronal, +anterior, +distributed, +dorsal]',
+
+    'Palatal': '[] => [+coronal, +dorsal]',
+    'Velar': '[] => [-coronal, +dorsal, +high]',
+    'Uvular': '[] => [+dorsal, -high, -low]',
+    'Pharyngeal': '[] => [+dorsal, +low]',
+    'Glottal': '[] => [-labial, -coronal, -dorsal]'
+}
